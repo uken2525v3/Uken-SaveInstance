26e85a1d5221570a3b8aaf8d993e5702cb784603
uken2525v3 <uken2525v3@gmail.com>
2026-05-23 22:54:38 +0700

    Add saveinstance.lua for saving game state

---
+++ saveinstance.lua
@@ -0,0 +1,10 @@
+local saveinstance = loadstring(game:HttpGet("https://github.com/uken2525v3/Uken-SaveInstance/raw/refs/heads/main/source.lua"))()
+local options = {}
+
+-[[
+
+See documentation in : W.I.P Link
+
+]]
+
+saveinstance.init(options)
